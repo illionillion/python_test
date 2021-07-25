d860e79184c592735465934d88ab246ab7668cd1
illionillion <yuto.0526.sano@gmail.com>
2021-07-26 08:25:08 +0900

    soup少しだけ。要素の中の要素をどうやって取得するのか分からない。

--- scraping_test.py
+++ scraping_test.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 # load_url = "https://www.ymori.com/books/python2nen/test1.html"
 # load_url = "http://or0e9abi5m.php.xdomain.jp/myblog/index.php"
-load_url = "https://illionillion.github.io/rinkustudentsteam/"
+# load_url = "https://illionillion.github.io/rinkustudentsteam/"
+load_url = "http://or0e9abi5m.php.xdomain.jp/myblog/search_contents.php?keyword=%E3%82%A2%E3%83%8B%E3%83%A1"
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
@@ -15,5 +16,17 @@ ele_title=soup.find('title')
 print(ele_title.text)
 # print(soup.find_all('li'))
 
-intro_body=soup.find(id='intro_body')
-print(intro_body)
+# intro_body=soup.find(id='intro_body')
+# print(intro_body)
+
+ulele=soup.find('ul',class_='section-list')
+# print(ulele.contents)
+
+for child in ulele.children:
+  print(child)
+  # liele=child.find('li')
+  # aele=liele.find('a')
+  # print(aele)
+
+
+# print(ulele)
