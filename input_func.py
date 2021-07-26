b0340a91c3ac95f022874ef426c124abca57ba43
illionillion <yuto.0526.sano@gmail.com>
2021-07-26 12:26:59 +0900

    修正した

--- input_func.py
+++ input_func.py
@@ -9,10 +9,10 @@ while not val_age.isdigit():#notで否定
   print('数値が入力されていません')
   print('年齢を入力してください')
   val_age=input()
-else:
-  print('こんにちわ、'+val_age+'歳の'+val_name+'さん')
-  import os
-  path='./txt/'+val_name+'.txt'
-  f=open(path,'w')
-  f.write('name:'+val_name+'\n'+'age:'+val_age+'')#名前:、年齢:と日本語にしたらVScodeで見る時に文字化けるメモ帳なら文字化けしない
-  f.close()
+
+print('こんにちわ、'+val_age+'歳の'+val_name+'さん')
+import os
+path='./txt/'+val_name+'.txt'
+f=open(path,'w')
+f.write('name:'+val_name+'\n'+'age:'+val_age+'')#名前:、年齢:と日本語にしたらVScodeで見る時に文字化けるメモ帳なら文字化けしない
+f.close()
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 print("hello world")
 
-x=10
 import os
 dir='./txt'
-if(not os.path.exists(dir)):
+if not os.path.exists(dir):#()いらない
   os.mkdir('./txt')#フォルダの作成
 
+x=10
 for y in range(x):
   print(y)
   path='./txt/'+str(y)+'.txt'
