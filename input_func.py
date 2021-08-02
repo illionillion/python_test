2babea7a9715fba7ef81e3a7e15aa755f4d0a47d
illionillion <yuto.0526.sano@gmail.com>
2021-08-03 07:39:47 +0900

    openするときに文字コードをutf-8に指定して文字化けしないようにした。

--- input_func.py
+++ input_func.py
@@ -13,6 +13,6 @@ while not val_age.isdigit():#notで否定
 print('こんにちわ、'+val_age+'歳の'+val_name+'さん')
 import os
 path='./txt/'+val_name+'.txt'
-f=open(path,'w')
-f.write('name:'+val_name+'\n'+'age:'+val_age+'')#名前:、年齢:と日本語にしたらVScodeで見る時に文字化けるメモ帳なら文字化けしない
+f=open(path,'w',encoding='utf-8')#encoding='utf-8'文字コード指定
+f.write('名前:'+val_name+'\n'+'年齢:'+val_age+'')#名前:、年齢:と日本語にしたらVScodeで見る時に文字化けるメモ帳なら文字化けしない
 f.close()
